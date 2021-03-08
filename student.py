166909011ff28111cc3fdaef1dc5acb04da42e60
sauravsingh2 <80242105+sauravsingh2@users.noreply.github.com>
2021-03-08 14:48:56 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,1 +1,19 @@
 
+class Student:
+   
+   
+    def __init__(self,name,Id,percentage,skills):
+        self.name = name
+        self.Id = Id
+        self.percentage = percentage
+        self.skills = skills
+       
+    def get_name(self):
+        return self.name
+   
+    def get_Id(self):
+        return self.Id
+    def get_percentage(self):
+        return self.percentage
+    def get_skills(self):
+        return self.skills
