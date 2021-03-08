b53ce6baf1259f28e126f736951761769143c6af
sauravsingh2 <80242105+sauravsingh2@users.noreply.github.com>
2021-03-08 14:55:45 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,25 +1,27 @@
-
 class Student:
-    def __init__(self,name,Id,percentage = 0,skills = []):
+    def __init__(self, name, Id, percentage, skills):
         self.name = name
         self.Id = Id
         self.percentage = percentage
         self.skills = skills
-       
+
     def get_name(self):
         return self.name
-   
+
     def get_Id(self):
         return self.Id
+
     def get_percentage(self):
         return self.percentage
+
     def get_skills(self):
         return self.skills
-   def set_name(self, name):
-      self.name = name
-        
-    def set_percentage(self, perct);
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_percentage(self, perct):
         self.percentage = perct
-        
+
     def set_skill(self, skillsnew):
         self.skills = skillsnew
